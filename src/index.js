2b673435bed0769b086697192479bac16d2918cd
Garan Guillory <garanguillory@gmail.com>
2016-09-11 17:23:05 -0500

    Syncing redux dev tools.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
 
+const enhancers = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(reducers, enhancers)}>
     <App />
   </Provider>
   , document.querySelector('.container'));
